510d6a0bd379fad31f638628bfb1a63d0c1d86e4
VitoorFranca <franc.vitoor@gmail.com>
2025-08-05 01:06:05 -0300

    Atualiza o workflow de deploy para verificar a existência do diretório do app, realizando pull das mudanças se já existir e fazendo backup da pasta SSL. Adiciona instruções para agendamento automático de renovação de SSL no script certbot-renew.sh e atualiza o .gitignore para incluir a pasta de certificados SSL.

--- scripts/certbot-renew.sh
+++ scripts/certbot-renew.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # renovar-ssl.sh
 
+# Para agendar a renovação automática, adicione a seguinte linha ao seu crontab (crontab -e):
+# 0 3 1 */2 * /c/Users/Vitor/Desktop/Projects/elven-sre/api/scripts/certbot-renew.sh >> /c/Users/Vitor/Desktop/Projects/elven-sre/api/scripts/certbot-renew.log 2>&1
+
 # Parar Nginx
 docker-compose stop nginx
 
